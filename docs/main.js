157dc5134cb78f840b22fffe13822ce56a732b75
nightblade9 <nightbladecodes@gmail.com>
2021-02-15 18:31:06 -0500

    Check for app IDs in query string, write to output

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,30 @@
 main = () => {
-    console.log("Hello from main.js!")
+    const STEAM_APP_URL = "https://store.steampowered.com/app/" // append app_id
+
+    // Sort by newest-first, up to the max (100 per page). See: https://partner.steamgames.com/doc/store/getreviews
+    const STEAM_BASE_URL = "https://store.steampowered.com/appreviews/{}?json=1&filter=recent&num_per_page=100"
+
+    const OUTPUT_ELEMENT_ID = "content"
+    
+    var output = "";
+    var queryParameters = window.location.search.slice(1).split("&");
+    var foundAppIds = false;
+
+    for (var i = 0; i < queryParameters.length; i++)
+    {
+        var params = queryParameters[i].split('=');
+        var key = params[0];
+        var value = params[1];
+        if (key.toLowerCase() == "appids") {
+            foundAppIds = true;
+            break;
+        }
+    }
+
+    if (!foundAppIds)
+    {
+        output = "No app IDs found, please append them to the URL, e.g. ?appIds=123456,789098"
+    }
+
+    document.getElementById(OUTPUT_ELEMENT_ID).innerHTML = output;
 }
